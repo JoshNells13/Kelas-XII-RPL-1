0a87dda941f810d4e00c7b8e9a8526a75527960a
Joshua CL <joshua.plk08@gmail.com>
2025-08-20 20:04:52 +0700

    Fix Json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base : '/Kelas-XII-RPL-1/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
